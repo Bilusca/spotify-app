4ba89b93994c096d30d674b882dc7be38706447a
Bilusca <lucasav2005@hotmail.com>
2017-10-18 00:08:07 -0200

    Adding AlbumTracks Test and AlbumTracks src in a project

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifWrapper({
-  token: 'BQABskKz--YXE2_ijGgVgRLrSVfVHW1LKf_Pt6FhCgBRrF4BcgjCn-bx6rJBl2npTEWPYVeK-ohO5U_SZMG4SGiMzlRESNCRz2CMyYeUja__03s3khAw2lshmrFYk1ILLEYN0E8gVUn4W7OZ'
+  token: 'BQBOqiNrCAYyRpWueoNuJrGuUHu6w93M-0BNexPGKRhA1YzmSkKM-7-_gW5suCsgMHVT2IVgxfb1KZZVTQDGsYskb1o3-YX16TMPcVHDRXTyc_K7IaLhKTxPnNeW7za_gTN-bIsuQN07tqNr'
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import spotify from './Spotify';
 
 import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
 
 const albums = spotify.search.albums('Kanye West');
 const albumList = document.getElementById('album-list');
 
 const album = spotify.album.getAlbum('4xM1pUHZp9HsuKNxyOQDR0');
 const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-tracks');
 
 albums
   .then( data => renderAlbums(data.albums.items, albumList));
 
 album
-  .then( data => renderAlbumInfo(data, albumInfo));
+  .then( data => renderAlbumInfo(data, albumInfo))
+  .then( data => renderAlbumTracks(data.tracks.items, albumTracks));
